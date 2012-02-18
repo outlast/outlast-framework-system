aa1b8addf13c6e1cbcadd0a5b4400fef552fb0a0
zajmedia <zajmedia@1177bc47-7a3e-47f4-a2ea-9a189b2012c5>
2012-02-18 21:04:57 +0000

    New request library with curl support.
    git-svn-id: https://outlast.svn.beanstalkapp.com/mozajik-private/trunk/system@27 1177bc47-7a3e-47f4-a2ea-9a189b2012c5

--- plugins/_global/library/url.lib.php
+++ plugins/_global/library/url.lib.php
@@ -85,75 +85,7 @@ class zajlib_url extends zajLibExtension {
 	 	// return @eregi('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$', $email);
 	 	return preg_match("/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/", $email);
 	}
-	
-	/**
-	 * Sends a POST request to a specified url, by using the query string as post data. Supports HTTPS.
-	 * @param string $url The url of the desired destination. Example: send_post("https://www.mozajik.org/akarmi.php?asdf=1&qwerty=2");
-	 * @param bool $returnheaders If set to true, the headers will be returned as well. By default it is false, so only document content is returned.
-	 **/
-	function send_post($url, $returnheaders = false){
-		// parse the url
-			$urldata = parse_url($url);
-			if($urldata === false) return false;
-		// now send the POST request and return the result
-			return $this->send_request($url, $urldata['query'], 'POST', array('Content-type'=>'application/x-www-form-urlencoded'), $returnheaders);		
-	}
-	
-	/**
-	 * Sends a request via GET or POST method to the specified url. Supports HTTPS.
-	 * @param string $url The url of the desired destination.
-	 * @param string $content The content of the document to be sent.
-	 * @param string $method Specifies the method by which the content is sent. Can be GET (the default) or POST.
-	 * @param array $customheaders An array of keys and values with custom headers to be sent along with the content.
-	 * @param bool $returnheaders If set to true, the headers will be returned as well. By default it is false, so only document content is returned.
-	 **/
-	function send_request($url, $content, $method = 'GET', $customheaders = false, $returnheaders = false){
-		// parse the url
-			$urldata = parse_url($url);
-			if($urldata === false) return false;
 		
-		// get port
-			if($urldata['scheme'] == "https"){
-				$port = 443;
-				$prefix = "ssl://";
-			}
-			else $port = 80;
-		// get method
-			 if($method == 'POST') $method = 'POST';
-			 else $method = 'GET';
-		// assemble my headers (if none given)
-			if(empty($customheaders)) $customheaders = array();
-			if(!is_array($customheaders)) return $this->zajlib->error("Invalid format for custom headers! Must be a key/value array.");
-			if(empty($customheaders['Content-type']) && empty($customheaders['content-type']) && empty($customheaders['Content-Type'])) $customheaders['Content-type'] = "text/html";
-		// open remote host
-			$fp = fsockopen($prefix.$urldata['host'], $port);
-			if($fp === false) return false;
-		// send GET or POST request
-			fputs($fp, "$method $urldata[path] HTTP/1.1\r\n");
-			fputs($fp, "Host: $urldata[host]\r\n");
-			// Send custom headers
-				foreach($customheaders as $key=>$value) fputs($fp, "$key: $value\r\n");
-		// send the content		
-			fputs($fp, "Content-length: ".strlen($content)."\r\n");
-			fputs($fp, "Connection: close\r\n\r\n");
-			fputs($fp, $content."\r\n\r\n");
-		// get response
-			$buf = '';
-			while (!feof($fp)) $buf.=fgets($fp,102);
-		// close connection
-			fclose($fp);
-		
-		// now split into header and content
-			$bufdata = explode("\r\n\r\n", $buf);
-			$headers = $bufdata[0];
-			$content = $bufdata[1];
-		
-		// now return what was requested
-		if($returnheaders) return $buf;
-		else return $content;
-	
-	}
-	
 	/**
 	 * Redirects to a URL based on the current subdomain.
 	 * @param string $from The subdomain to check for.
@@ -169,6 +101,22 @@ class zajlib_url extends zajLibExtension {
 		}
 		return false;
 	}
+
+
+	/**
+	 * Depricated. Use {@link zajlib_request->post()} instead.
+	 **/
+	function send_post($url, $returnheaders = false){
+		$this->zajlib->request->post($url, '', $returnheaders);
+	}
+	
+	/**
+	 * Depricated. Use {@link zajlib_request->get()} instead.
+	 **/
+	function send_request($url, $content, $method = 'GET', $customheaders = false, $returnheaders = false){
+		$this->zajlib->request->get($url, $content, $returnheaders, $customheaders, $method);
+	}
+
 }
 
 
