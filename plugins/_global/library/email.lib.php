94fcbdc266891ccf8b269f5ca1d1776559178959
zajmedia <zajmedia@1177bc47-7a3e-47f4-a2ea-9a189b2012c5>
2012-09-01 08:34:31 +0000

    Allowing new top level domains (TLDs) in email validation script.
    git-svn-id: https://outlast.svn.beanstalkapp.com/mozajik-private/trunk/system@132 1177bc47-7a3e-47f4-a2ea-9a189b2012c5

--- plugins/_global/library/email.lib.php
+++ plugins/_global/library/email.lib.php
@@ -106,7 +106,7 @@ class zajlib_email extends zajLibExtension {
 				$email = $email_data->email;
 			}
 		// Now check and return 
-			return @eregi('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$', $email);
+			return @eregi('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$', $email);
 	}
 }
 
