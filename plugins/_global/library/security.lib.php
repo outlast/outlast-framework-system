08ace341a433a79dc758cd2962c5e958787a2dcd
zajmedia <zajmedia@1177bc47-7a3e-47f4-a2ea-9a189b2012c5>
2012-11-06 12:34:24 +0000

    Adding ip address verification.
    git-svn-id: https://outlast.svn.beanstalkapp.com/mozajik-private/trunk/system@182 1177bc47-7a3e-47f4-a2ea-9a189b2012c5

--- plugins/_global/library/security.lib.php
+++ plugins/_global/library/security.lib.php
@@ -48,6 +48,68 @@ class zajlib_security extends zajLibExtension {
 		// Retun the password we've selected
 			return $pass;
 	}
+
+	
+	/**
+	 * Checks if an IP address is within the specified range.
+	 * 
+	 * Network ranges can be specified as:
+	 * 1. Wildcard format:     1.2.3.*
+	 * 2. CIDR format:         1.2.3/24  OR  1.2.3.4/255.255.255.0
+	 * 3. Start-End IP format: 1.2.3.0-1.2.3.255
+	 * The function will return true if the supplied IP is within the range.
+	 *
+	 * @param string $ip The ip address to check.
+	 * @param string $range The ip address range to check in.
+	 * @return Will return true if the specified IP is within the given range.
+	 **/
+	 function ip_in_range($ip, $range) {
+		if (strpos($range, '/') !== false) {
+		    // $range is in IP/NETMASK format
+		    list($range, $netmask) = explode('/', $range, 2);
+		    if (strpos($netmask, '.') !== false) {
+		      // $netmask is a 255.255.0.0 format
+		      $netmask = str_replace('*', '0', $netmask);
+		      $netmask_dec = ip2long($netmask);
+		      return ( (ip2long($ip) & $netmask_dec) == (ip2long($range) & $netmask_dec) );
+		    } else {
+		      // $netmask is a CIDR size block
+		      // fix the range argument
+		      $x = explode('.', $range);
+		      while(count($x)<4) $x[] = '0';
+		      list($a,$b,$c,$d) = $x;
+		      $range = sprintf("%u.%u.%u.%u", empty($a)?'0':$a, empty($b)?'0':$b,empty($c)?'0':$c,empty($d)?'0':$d);
+		      $range_dec = ip2long($range);
+		      $ip_dec = ip2long($ip);
+
+		      # Strategy 1 - Create the netmask with 'netmask' 1s and then fill it to 32 with 0s
+		      #$netmask_dec = bindec(str_pad('', $netmask, '1') . str_pad('', 32-$netmask, '0'));
+
+		      # Strategy 2 - Use math to create it
+		      $wildcard_dec = pow(2, (32-$netmask)) - 1;
+		      $netmask_dec = ~ $wildcard_dec;
+
+		      return (($ip_dec & $netmask_dec) == ($range_dec & $netmask_dec));
+		    }
+		  } else {
+		    // range might be 255.255.*.* or 1.2.3.0-1.2.3.255
+		    if (strpos($range, '*') !==false) { // a.b.*.* format
+		      // Just convert to A-B format by setting * to 0 for A and 255 for B
+		      $lower = str_replace('*', '0', $range);
+		      $upper = str_replace('*', '255', $range);
+		      $range = "$lower-$upper";
+		    }
+
+		    if (strpos($range, '-')!==false) { // A-B format
+		      list($lower, $upper) = explode('-', $range, 2);
+		      $lower_dec = (float)sprintf("%u",ip2long($lower));
+		      $upper_dec = (float)sprintf("%u",ip2long($upper));
+		      $ip_dec = (float)sprintf("%u",ip2long($ip));
+		      return ( ($ip_dec>=$lower_dec) && ($ip_dec<=$upper_dec) );
+		    }
+		    return false;
+		}
+	}
 }
 
 
