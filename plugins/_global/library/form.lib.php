1528adf3bb3e022369c3c2b96ee86b897a26ab9d
Aron Budinszky <aron@outlast.hu>
2013-05-12 18:38:33 +0200

    Fixing docs and updating form validation.

--- plugins/_global/library/form.lib.php
+++ plugins/_global/library/form.lib.php
@@ -10,11 +10,11 @@ class zajlib_form extends zajLibExtension {
 
 		/**
 		 * Check require fields - will return false if any of the fields specified were not filled out (are empty).
-		 * @param string $field1 The name of the first field.
-		 * @param string $field2 The name of the second field.
-		 * @return Return true if all are filled out, false if not.
+		 * @internal param string $field1 The name of the first field.
+		 * @internal param string $field2 The name of the second field.
+		 * @return boolean Return true if all are filled out, false if not.
 		 * @todo Autoselect POST or GET instead of using REQUEST.
-		 **/
+		 */
 		public function filled(){
 			// Get function arguments
 				$arguments = func_get_args();
@@ -34,10 +34,16 @@ class zajlib_form extends zajLibExtension {
 		 * Validate fields and automatically return a result and stop execution.
 		 * @param string $class_name The zajModel class whos fields we are checking.
 		 * @param array $fields An array of strings with the field names.
-		 * @param array $request The request which to check against. By default POST or GET is automatically checked, based on which mode we are in.
-		 * @return Print true if all are filled out, false if not.
+		 * @param array|bool $request The request which to check against. By default POST or GET is automatically checked, based on which mode we are in.
+		 * @return boolean Print true if all are filled out, false if not.
+		 * @todo Implement this
 		 **/
 		public function validate($class_name, $fields, $request = false){
+
+			// NOT YET IMPLEMENTED!
+			return true;
+
+			/**
 			// Get function arguments
 				$arguments = func_get_args();
 			// Run through all and make sure each is not empty
@@ -48,18 +54,7 @@ class zajlib_form extends zajLibExtension {
 						break;
 					}
 				}
-			return $valid_input;
-		}
-
-		/**
-		 * Validate fields and return the result. This is same as validate() but will not stop execution so you can customize the error.
-		 * @param string $class_name The zajModel class whos fields we are checking.
-		 * @param array $fields An array of strings with the field names.
-		 * @param array $request The request which to check against. By default POST or GET is automatically checked, based on which mode we are in.
-		 * @return Print true if all are filled out, false if not.
-		 **/
-		public function check($class_name, $fields, $request = false){
-
+			return $valid_input;**/
 		}
 
 }
