d2f8bd0616c985733ad4497db55ff32c03edff80
Aron Budinszky <aron@outlast.hu>
2013-07-22 09:23:04 +0200

    Timestamp type now supports unix timestamps.

--- plugins/_mootools/fields/timestamp.field.php
+++ plugins/_mootools/fields/timestamp.field.php
@@ -8,7 +8,7 @@ class zajfield_timestamp extends zajField {
 	// name, options - these are passed to constructor and available here!
 	const in_database = true;		// boolean - true if this field is stored in database		
 	const use_validation = false;	// boolean - true if data should be validated before saving
-	const use_get = false;			// boolean - true if preprocessing required before getting data
+	const use_get = true;			// boolean - true if preprocessing required before getting data
 	const use_save = true;			// boolean - true if preprocessing required before saving data
 	const use_duplicate = true;		// boolean - true if data should be duplicated when duplicate() is called
 	const use_filter = false;		// boolean - true if fetch is modified
@@ -53,9 +53,9 @@ class zajfield_timestamp extends zajField {
 	
 	/**
 	 * Preprocess the data before returning the data from the database.
-	 * @param $data The first parameter is the input data.
+	 * @param mixed $data The first parameter is the input data.
 	 * @param zajModel $object This parameter is a pointer to the actual object which is being modified here.
-	 * @return Return the data that should be in the variable.
+	 * @return integer Return the data that should be in the variable.
 	 **/
 	public function get($data, &$object){
 		// turn date into unix date
@@ -66,27 +66,18 @@ class zajfield_timestamp extends zajField {
 	
 	/**
 	 * Preprocess the data before saving to the database.
-	 * @param $data The first parameter is the input data.
+	 * @param mixed $data The first parameter is the input data.
 	 * @param zajModel $object This parameter is a pointer to the actual object which is being modified here.
 	 * @return array Returns an array where the first parameter is the database update, the second is the object update
 	 * @todo Fix where second parameter is actually taken into account! Or just remove it...
 	 * @todo Remove display/format version
 	 **/
 	public function save($data, &$object){
-		if(is_array($data)){
-			// date[format] and date[display] (backwards compatible
-			if(!empty($data['format'])){
-				$dt = date_create_from_format($data['format'], $data['display']);
-				if(is_object($dt)) $data = $dt->getTimestamp();
-				else $data = '';
-			}
-			else{
-				$data = $data['value'];
-			}
+		// Convert a unix timestamp into the proper format
+		if(is_numeric($data)){
+			$datetime = new DateTime();
+			$data = $datetime->setTimestamp($data)->format("Y-m-d H:i:s");
 		}
 		return array($data, $data);
 	}
 }
-
-
-?>
