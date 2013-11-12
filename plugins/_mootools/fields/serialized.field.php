c89fa722f07987b4246ee6ce52e12f23d1456cc8
Aron Budinszky <aron@outlast.hu>
2013-11-12 17:28:39 +0100

    Doc fix.

--- plugins/_mootools/fields/serialized.field.php
+++ plugins/_mootools/fields/serialized.field.php
@@ -65,7 +65,7 @@ class zajfield_serialized extends zajField {
 	
 	/**
 	 * Preprocess the data before saving to the database.
-	 * @param $data The first parameter is the input data.
+	 * @param mixed $data The first parameter is the input data.
 	 * @param zajModel $object This parameter is a pointer to the actual object which is being modified here.
 	 * @return array Returns an array where the first parameter is the database update, the second is the object update
 	 * @todo Fix where second parameter is actually taken into account! Or just remove it...
