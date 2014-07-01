f07d0e9e9eaec8ef4d6d61fed1a58a4d7d3ccfab
Aron Budinszky <aron@outlast.hu>
2014-07-01 20:58:42 +0200

    Add {{field.choices_toplevel}} to type category.

--- plugins/_jquery/fields/manytoone.field.php
+++ plugins/_jquery/fields/manytoone.field.php
@@ -162,13 +162,14 @@ class zajfield_manytoone extends zajField {
 	 * This method is called just before the input field is generated. Here you can set specific variables and such that are needed by the field's GUI control.
 	 * @param array $param_array The array of parameters passed by the input field tag. This is the same as for tag definitions.
 	 * @param zajCompileSource $source This is a pointer to the source file object which contains this tag.
+	 * @return bool
 	 **/
 	public function __onInputGeneration($param_array, &$source){
 		// override to print all choices
 			// use search method with all			
 				$class_name = $this->options['model'];
 			// write to compile destination
-				$this->zajlib->compile->write('<?php $this->zajlib->variable->field->choices = '.$class_name.'::__onSearch('.$class_name.'::fetch()); if($this->zajlib->variable->field->choices === false) $this->zajlib->warning("__onSearch method required for '.$class_name.' for this input."); ?>');
+				$this->zajlib->compile->write('<?php $this->zajlib->variable->field->choices = '.$class_name.'::__onSearch('.$class_name.'::fetch()); $this->zajlib->variable->field->choices_toplevel = '.$class_name.'::__onSearch('.$class_name.'::fetch_top_level()); if($this->zajlib->variable->field->choices === false) $this->zajlib->warning("__onSearch method required for '.$class_name.' for this input."); ?>');
 		return true;
 	}
 
