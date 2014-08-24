d8465f45febcc122f71a72d9560b9d1301cb5d3c
Aron Budinszky <aron@outlast.hu>
2014-08-25 00:21:21 +0200

    is_a($obj, 'zajModel') fix. This needs to be fixed elsewhere too. See OP-1580.

--- plugins/_jquery/fields/manytoone.field.php
+++ plugins/_jquery/fields/manytoone.field.php
@@ -150,7 +150,7 @@ class zajfield_manytoone extends zajField {
 			}
 			else{
 				// Possible values: object, string, boolean false
-					if(is_object($value) && is_a($value, 'zajModel')) $value = $value->id;
+					if(is_object($value) && (is_a($value, 'zajModel') || is_a($value, 'zajModelExtender'))) $value = $value->id;
 					elseif($value === false) return "0"; // Return no filter if boolean false
 					elseif(!is_string($value) && !is_integer($value)) return $this->zajlib->error("Invalid value given for filter/exclude of fetcher object for $this->class_name/$field! Must be a string, a model object, or a fetcher object!");
 				// All is ok, now simply return
