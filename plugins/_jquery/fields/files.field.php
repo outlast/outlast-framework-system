f8b56b3aa9991a19a34b662bcdcddc2d47fc343b
Bálint Szilva <balint@outlast.hu>
2015-01-12 13:50:48 +0100

    files filter method

--- plugins/_jquery/fields/files.field.php
+++ plugins/_jquery/fields/files.field.php
@@ -10,7 +10,7 @@ class zajfield_files extends zajField {
 	const use_validation = false;	// boolean - true if data should be validated before saving
 	const use_get = true;			// boolean - true if preprocessing required before getting data
 	const use_save = true;			// boolean - true if preprocessing required before saving data
-	const use_filter = false;		// boolean - true if fetch is modified
+	const use_filter = true;		// boolean - true if fetch is modified
 	const search_field = false;		// boolean - true if this field is used during search()
 	const edit_template = 'field/files.field.html';	// string - the edit template, false if not used
 	const show_template = false;	// string - used on displaying the data via the appropriate tag (n/a)
@@ -90,4 +90,37 @@ class zajfield_files extends zajField {
 		return array(false, false);
 	}
 
+    /**
+     * This is called when a filter() or exclude() methods are run on this field. It is actually executed only when the query is being built.
+     * @param zajFetcher $fetcher A pointer to the "parent" fetcher which is being filtered.
+     * @param array $filter An array of values specifying what type of filter this is.
+     **/
+    public function filter(&$fetcher, $filter){
+        // break up filter
+
+        list($field, $value, $logic, $type) = $filter;
+        // other fetcher's field
+        $other_field = 'parent';
+
+        // if value is a fetcher
+        if(is_object($value) && is_a($value, 'zajFetcher')){
+            // get my other query
+            $other_fetcher = $value->limit(false)->sort(false);
+            // add field source
+            $other_fetcher->add_field_source('model.'.$other_field, 'other_field', true);
+        }
+        // else value is an id
+        else{
+            $model = $this->options['model'];
+            $other_fetcher = $model::fetch();
+            // filter the other fetcher
+            $other_fetcher->filter('id', $value)->limit(false)->sort(false);
+            $other_fetcher->add_field_source('model.'.$other_field, 'other_field', true);
+        }
+        // add source
+        $as_name = strtolower('sub_'.$this->class_name.'_'.$this->options['model'].'_'.$this->name);
+        $fetcher->add_source('('.$other_fetcher->get_query().')', $as_name);
+        // create local query
+        return "$as_name.other_field = model.id";
+    }
 }
