aa1f7580fe3bed4b63a91a3c631bf5b8781ff4d5
Bálint Szilva <balint@outlast.hu>
2015-03-25 19:49:16 +0100

    add API php example in comment to library

--- plugins/_global/library/signup.lib.php
+++ plugins/_global/library/signup.lib.php
@@ -4,6 +4,45 @@
  * @author Aron Budinszky <aron@outlast.hu>
  * @version 3.0
  * @package Library
+ *
+ * API PHP File example
+ * header('Access-Control-Allow-Origin: *');
+
+// Webgalamb MySQL fájl meghívása (helyes útvonalat neked kell tudnod!)
+require_once('./files/wg5conf.php');
+
+// Webgalamb 5 API meghívása
+require_once('wg5api.php');
+$wg_api = new WG5_API($db_pre);
+
+$name_key = utf8_encode('Teljes név');
+
+// Rögzítendo adatok.
+$subscriber_data = array (
+// E-mail cím
+'mail' => $_POST['email'],
+
+// Feliratkozási dátum, ha nincs megadva akkor az aktuális dátum kerül be.
+'datum' => date('Y-m-d'),
+
+// Státusz: 1 -> aktív, 0 -> inaktív, 2 -> visszapattant
+// Ha nincs megadva akkor aktív státusszal kerül be
+'active' => 1,
+
+$name_key => $_POST['name']
+//'Telefonszám' => '06-70/123-4567',
+//'Cégnév' => 'Minta Kft.'
+);
+
+// A feliratkozási csoport csoportazonosítója.
+$group_id = $_POST['group'];
+
+
+//
+$result = $wg_api->InsertSubscriber($subscriber_data, $group_id);
+
+// Eredmény kiírása, feldolgozása.
+echo $result; 
  **/
 
 class zajlib_signup extends zajLibExtension {
