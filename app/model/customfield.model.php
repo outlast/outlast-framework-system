4fa69a866fa5fed33a80089fc5d6bc835ceec458
Bálint Szilva <balint@outlast.hu>
2015-09-09 09:13:47 +0200

    add customfield and customfieldentry models

---
+++ app/model/customfield.model.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * A class for storing custom fields
+ */
+class CustomField extends zajModel
+{
+
+	///////////////////////////////////////////////////////////////
+	// !Model design
+	///////////////////////////////////////////////////////////////
+	public static function __model()
+	{
+		// define custom database fields
+		$f = (object)array();
+		$f->name = zajDb::name();
+		$f->type = zajDb::select(array('text', 'number'));
+		$f->featured = zajDb::boolean();
+		$f->customfieldentries = zajDb::onetomany('CustomFieldEntry', 'customfield');
+
+		// do not modify the line below!
+		$f = parent::__model(__CLASS__, $f);
+		return $f;
+	}
+
+	/**
+	 * Construction and required methods
+	 */
+	public function __construct($id = "")
+	{
+		parent::__construct($id, __CLASS__);
+		return true;
+	}
+
+	public static function __callStatic($name, $arguments)
+	{
+		array_unshift($arguments, __CLASS__);
+		return call_user_func_array(array('parent', $name), $arguments);
+	}
+
+	///////////////////////////////////////////////////////////////
+	// !Custom methods
+	///////////////////////////////////////////////////////////////
+
+	public function __afterFetch()
+	{
+	}
+}
