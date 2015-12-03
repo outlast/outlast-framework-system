bd4e55349b3ce13b4a780fde7844eeb983030f9b
Tibor Németh <tibor@outlast.hu>
2015-12-03 12:28:52 +0100

    + Zaj.htmlEscape + htmlUnescape added

--- site/js/mozajik-base-jquery.js
+++ site/js/mozajik-base-jquery.js
@@ -1201,6 +1201,23 @@
 			}
 		};
 
+        zaj.htmlEscape = function(str) {
+            return String(str)
+                .replace(/&/g, '&amp;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;');
+        }
+
+        zaj.htmlUnescape = function(str) {
+            return String(str)
+                .replace(/&quot;/g, '"')
+                .replace(/&#39;/g, "'")
+                .replace(/&lt;/g, '<')
+                .replace(/&gt;/g, '>')
+                .replace(/&amp;/g, '&');
+        }
 
 	/**
 	 * Pushstate excitement
