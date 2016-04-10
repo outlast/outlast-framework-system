fa1e3c71b95211366fa6c21193434300ef68b4d9
Aron Budinszky <aron@outlast.io>
2016-04-11 00:41:02 +0200

    Add parameter now concatenates when non-numerical values are used.

--- plugins/_global/filters/base.filters.php
+++ plugins/_global/filters/base.filters.php
@@ -23,10 +23,10 @@ class zajlib_filter_base extends zajElementCollection{
 	 **/
 	public function filter_add($parameter, &$source){
 		// validate parameter
-			$parameter = (trim($parameter,"'\""));
-			if(substr($parameter, 0, 1) != '$' && !is_numeric($parameter)) return $source->warning('add filter parameter not a variable or an integer!');
+			//$parameter = (trim($parameter,"'\""));
+			//if(substr($parameter, 0, 1) != '$' && !is_numeric($parameter)) return $source->warning('add filter parameter not a variable or an integer!');
 		// write to file
-			$this->zajlib->compile->write('$filter_var=$filter_var+'.$parameter.';');
+			$this->zajlib->compile->write('if(is_numeric($filter_var) && is_numeric('.$parameter.')) $filter_var=$filter_var+'.$parameter.'; else $filter_var=$filter_var.'.$parameter.';');
 		return true;
 	}
 	/**
