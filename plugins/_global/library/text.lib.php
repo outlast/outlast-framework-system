4b782a86df54f39ccdcd1125c3e4320ffe54cc1c
Tibor Németh <tibor@outlast.hu>
2016-04-12 11:46:35 +0200

    + convert_to_snake_case function added to text.lib.php + convert_to_camel_case function added to text.lib.php

--- plugins/_global/library/text.lib.php
+++ plugins/_global/library/text.lib.php
@@ -153,4 +153,40 @@ class zajlib_text extends zajLibExtension {
 		return mb_convert_case(mb_strtolower($str), MB_CASE_TITLE);
 	}
 
+	/**
+	 *
+	 * Convert string into snake_case formatted string
+	 *
+	 * @param string $input Input string
+	 * @return string snake_case formatted string
+	 */
+	function convert_to_snake_case($input) {
+		preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+		$ret = $matches[0];
+
+		foreach ($ret as &$match) {
+			$match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+		}
+
+		return implode('_', $ret);
+	}
+
+	/**
+	 *
+	 * Convert string into camelCase formatted string
+	 *
+	 * @param string $input Input string
+	 * @param boolean $pascal_case If true, the string will be formatted to PascalCase (instead of camelCase)
+	 * @return string camelCase formatted string
+	 */
+	function convert_to_camel_case($input, $pascal_case = false) {
+		$ret = str_replace(' ', '', ucwords(str_replace('_', ' ', $input)));
+
+		if (!$pascal_case) {
+			$ret = strtolower(substr($ret,0,1)) . substr($ret,1);
+		}
+
+		return $ret;
+	}
+
 }
