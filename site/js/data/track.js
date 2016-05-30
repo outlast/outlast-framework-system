cfb248fc3a3f74dbb378f89961ffb21d9184ab72
Bálint Szilva <balint@outlast.io>
2016-05-30 14:47:12 +0200

    track read events

--- site/js/data/track.js
+++ site/js/data/track.js
@@ -11,12 +11,42 @@ define('system/js/data/track', ["../ofw-jquery"], function() {
     var _objects = [];			// An array of autopagination objects on this page
 
     /** Private API **/
+    var defaultOptions = {
+        // Timeout of the delayed scroll event handling
+        addScrollCheckTimeout: 10000
+    };
+
+    var postOptions = {};
+    var options = {};
+
+    var dimensions = {};
+
+    // scroll events
+    var events = {
+        postScrollPercents: [
+            25,
+            50,
+            75,
+            100
+        ]
+    };
 
     /**
      * Object init
      */
     var init = function(){
-        // Nothing happens here for now, everything in public init.
+        // Merge default options
+        postOptions = $.extend(true, {}, defaultOptions, options);
+        // Set post height, position, and height of a post segment
+        setDimensions();
+
+        $(window).on('load resize', function() {
+            setDimensions();
+        });
+
+        window.addEventListener('orientationchange', function(){
+            setDimensions();
+        });
     };
 
     /** Public API **/
@@ -28,25 +58,72 @@ define('system/js/data/track', ["../ofw-jquery"], function() {
          * @param {jQuery} [$context=$(document)] The current jQuery object context in which the handlers are searched for.
          */
         activate: function($elements, $context) {
-            $elements.click(function(){
-                var el =  $(this);
-                var category = el.attr('data-track-category');
+            $elements.each(function(){
+                var $el =  $(this);
+                var category = $el.attr('data-track-category');
                 if(typeof(category) == 'undefined' || category == '') {
                     ofw.log('track.js: data-track-category attribute is required to send events.');
+                    return;
                 }
 
-                var action = el.attr('data-track-action');
+                var action = $el.attr('data-track-action');
                 if(typeof(action) == 'undefined' || action == '') {
                     ofw.log('track.js: data-track-action attribute is required to send events.');
+                    return;
                 }
+                var label = $el.attr('data-track-label');
 
-                var label = el.attr('data-track-label');
-
-                var value = el.attr('data-track-value');
-
-                ofw.track(category, action, label, value);
+                var value = $el.attr('data-track-value');
+                // track read events
+                if(action == 'read'){
+                    // Delayed call of addScrollCheck
+                    setTimeout(function(){
+                        addScrollCheck(category, action, label, value)
+                    }, postOptions.addScrollCheckTimeout);
+                }
+                // track click events
+                else{
+                    $el.click(function(){
+                        ofw.track(category, action, label, value);
+                    });
+                }
             });
         }
+    };
+
+    /**
+     * Set dimensions based on div.
+     */
+    var setDimensions = function() {
+        var $post = $('.article-body');
+        dimensions.postHeight = $post.height();
+        dimensions.postTop = $post.offset().top;
+    };
+
+    /**
+     * Add scroll event.
+     */
+    var addScrollCheck = function(category, action, label, value) {
+        $(window).scroll(function() {
+            var scroll_top = $(window).scrollTop();
+            if (events.postScrollPercents.length && scroll_top > dimensions.postTop && scroll_top < dimensions.postTop + dimensions.postHeight) {
+                // Initial post scroll percent
+                var percent = 0;
+
+                // Look through all the percents
+                for (var perc_idx in events.postScrollPercents) {
+                    percent = events.postScrollPercents[perc_idx];
+                    if (scroll_top >= dimensions.postHeight * percent / 100) {
+                        // Send GA event
+                        ofw.track(category, action, label, percent);
+                        // Mark as visited (remove from array)
+                        delete events.postScrollPercents[perc_idx];
+                    }
+                }
+            } else {
+                $(window).unbind('scroll');
+            }
+        });
     };
 
     /** Perform private initialization **/
