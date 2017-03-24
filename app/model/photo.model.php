06958f863e7dac91327a3288d9d5ae806b312047
Aron Budinszky <aron@outlast.io>
2017-03-24 23:53:30 +0100

    Use is_instance_of_me instead of is_object for checking if model.

--- app/model/photo.model.php
+++ app/model/photo.model.php
@@ -499,7 +499,7 @@ class Photo extends zajModel {
 	 */
 	public static function create_with_parent($parent, $field){
 		// Check parent object
-			if(!is_object($parent) || !is_a($parent, 'zajModel')){
+			if(!zajModel::is_instance_of_me($parent)){
 				// @todo Change this to an error once we are done updating code!
 				//return zajLib::me()->error("You tried to create a new Photo with an invalid parent. You must pass an object instead of an id.");
 
