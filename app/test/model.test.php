e7500aafa714d850816ef69f70db57d986eb5810
Aron Budinszky <aron@budinszky.me>
2018-11-02 23:00:26 +0100

    Formatting.

--- app/test/model.test.php
+++ app/test/model.test.php
@@ -22,6 +22,7 @@
             $this->category->set('name', 'mock!');
             $this->category->set('description', 'mymockdesc');
             $this->category->save();
+
             return true;
         }
 
